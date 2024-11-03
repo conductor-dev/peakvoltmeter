a6876554ddf2f3c20018fcc3e486c19771299c44
Alexander <alexander0153@users.noreply.github.com>
2024-11-03 13:28:28 +0100

    feat: plot rising edge trigger

---
+++ src/trigger.rs
@@ -0,0 +1,70 @@
+pub struct RisingEdgeTrigger<I, T>
+where
+    I: Iterator<Item = T>,
+    T: PartialOrd + Copy,
+{
+    iter: I,
+    threshold: T,
+    prev_value: Option<T>,
+    triggered: bool,
+}
+
+impl<I, T> RisingEdgeTrigger<I, T>
+where
+    I: Iterator<Item = T>,
+    T: PartialOrd + Copy,
+{
+    pub fn new(iter: I, threshold: T) -> Self {
+        RisingEdgeTrigger {
+            iter,
+            threshold,
+            prev_value: None,
+            triggered: false,
+        }
+    }
+}
+
+impl<I, T> Iterator for RisingEdgeTrigger<I, T>
+where
+    I: Iterator<Item = T>,
+    T: PartialOrd + Copy,
+{
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.triggered {
+            return self.iter.next();
+        }
+
+        while let Some(current_value) = self.iter.next() {
+            if let Some(prev_value) = self.prev_value {
+                if current_value > self.threshold && prev_value <= self.threshold {
+                    self.triggered = true;
+                    self.prev_value = Some(current_value);
+                    return Some(current_value);
+                }
+            }
+            self.prev_value = Some(current_value);
+        }
+
+        None
+    }
+}
+
+pub trait RisingEdgeTriggerExt<I, T>: Iterator<Item = T>
+where
+    I: Iterator<Item = T>,
+    T: PartialOrd + Copy,
+{
+    fn rising_edge_trigger(self, threshold: T) -> RisingEdgeTrigger<I, T>;
+}
+
+impl<I, T> RisingEdgeTriggerExt<I, T> for I
+where
+    I: Iterator<Item = T>,
+    T: PartialOrd + Copy,
+{
+    fn rising_edge_trigger(self, threshold: T) -> RisingEdgeTrigger<I, T> {
+        RisingEdgeTrigger::new(self, threshold)
+    }
+}
